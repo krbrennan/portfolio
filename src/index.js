d853c950c529176d58e46358e17497b163cc3e4c
krbrennan <krb.brennan@gmail.com>
2018-06-02 15:46:10 -0400

    Formatting project divs.

--- src/index.js
+++ src/index.js
@@ -14,8 +14,22 @@ let occupation = {
 let typed = new Typed('#typed', name);
 let typeName = new Typed('#type-name', occupation);
 
-
 document.addEventListener("scroll", function() {
+  let viewHeight = document.querySelector('.section_1').clientHeight;
   let nav = document.getElementById('navbar');
-  nav.classList.toggle('sticky', window.scrollY > 300);
+  nav.classList.toggle('sticky', window.scrollY > viewHeight);
 });
+
+
+// Smooth Scrolling //
+let about_link = document.getElementById('to-about');
+let about_projects = document.getElementById('to-projects');
+let project_section = document.getElementById('project-showcase');
+let about_section = document.getElementById('about-me');
+
+let handleClick = function(event) {
+  event.preventDefault();
+  smoothScroll(project_section);
+};
+
+about_projects.addEventListener("click", handleClick);
