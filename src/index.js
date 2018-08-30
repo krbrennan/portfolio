96c45f79b3fd6019881ca016bfc1dcc88660ddf9
krbrennan <krb.brennan@gmail.com>
2018-08-30 12:47:20 -0400

    Implemented my own smoothscroll after learning that it wasnt working on pages

--- src/index.js
+++ src/index.js
@@ -27,9 +27,13 @@ let about_projects = document.getElementById('to-projects');
 let project_section = document.getElementById('project-showcase');
 let about_section = document.getElementById('about-me');
 
-let handleClick = function(event) {
-  event.preventDefault();
-  smoothScroll(project_section);
-};
+// let handleClick = function(event) {
+//   event.preventDefault();
+//   smoothScroll(project_section);
+// };
 
-about_projects.addEventListener("click", handleClick);
+// about_projects.addEventListener("click", handleClick);
+
+about_projects.scrollIntoView({
+  behavior: 'smooth'
+});
